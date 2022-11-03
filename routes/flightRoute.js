626289b9de33be1ac97a1932f4ad9eb3804e9752
Emmanuel Olowoyo <ayleosoftsms@gmail.com>
2022-11-03 23:11:31 +0100

    update

--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -5,5 +5,20 @@ const controller = require('../controllers/flightController');
 
 router.get('/', controller.example)
 
+/*
+? Delete a flight
+*/
+router.delete('/:flightId', async (req, res) => {
+    try {
+        const removedflight = await flight.remove({_Id:req.params.flightId});
+        res.json(removedflight);
+    } catch (err) {
+        res.json({message:err});
+    });
+
+
+
+
+
 module.exports = router;
 
